d2e3a614f9595fca63fe9a88967b3ba327397d3b
asilkro <asilkro@me.com>
2022-06-02 22:03:37 -0700

    WIP - TODO: Build appt form

--- Forms/CustomerForm.cs
+++ Forms/CustomerForm.cs
@@ -27,6 +27,24 @@ namespace Silkroski_C969_Revised.Forms
         private void CxSaveButton_Click(object sender, EventArgs e)
         {
             //TODO: Implement validation, save
+            ValidCustomerData();
+        }
+
+        private bool ValidCustomerData() // Not elegant but addresses req F
+        {
+            if (CxNameTB.Text.Length == 0 || CxAddress1TB.Text.Length == 0 || CxCityTB.Text.Length == 0 
+                || CxCountryTB.Text.Length == 0 || CxPostalTB.Text.Length == 0 || CxPostalTB.Text.Length == 0)
+                // Skipping Address2 box since this would be apartment or po box numbers which are often empty
+            {
+                MessageBox.Show("Unable to save. Please check your data and retry.",
+                    "Data validation error");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
+
 }
